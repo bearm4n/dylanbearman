d012e991a4ed5d0192dee830138550a050a39f5e
dylanbearmanBBC <dylan.bearman@bbc.co.uk>
2023-02-03 12:40:07 +0000

    update welcome message

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Welcome to my blog</p>
+        <p>Hi, I'm Dylan Bearman. A Software Engineer at the BBC</p>
         <a
           className="App-link"
           href="https://reactjs.org"
